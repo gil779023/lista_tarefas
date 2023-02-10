a0033549df87eb61abed96cf6d6f92f07cfd4b44
gil779023 <115327502+gil779023@users.noreply.github.com>
2023-02-10 09:45:37 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -6,58 +6,59 @@ frm.addEventListener("submit", (e) => {
 
   const tarefa = frm.inTarefa.value             // obtém o conteúdo digitado
 
-  const h5 = document.createElement("h5")       // cria o elemento HTML h5
+  const h3 = document.createElement("h3")       // cria o elemento HTML h5
   const texto = document.createTextNode(tarefa) // cria um texto
-  h5.appendChild(texto)                         // define que texto será filho de h5
-  dvQuadro.appendChild(h5)                      // e que h5 será filho de divQuadro
+  h3.appendChild(texto)                         // define que texto será filho de h5
+  dvQuadro.appendChild(h3)                      // e que h5 será filho de divQuadro
 
   frm.inTarefa.value = ""                       // limpa o campo de edição
   frm.inTarefa.focus()                          // joga o cursor neste campo
 })
 
 frm.btSelecionar.addEventListener("click", () => { 
-  const tarefas = document.querySelectorAll("h5")  
+  const tarefas = document.querySelectorAll("h3")     // cria um click na variavel
 
   if (tarefas.length == 0) {
-    alert("Não há tarefas para selecionar")       
+    alert("Não há tarefas para selecionar")           // compara se variavel igual a zero
     return                                        
   }
 
-  let aux = -1                   
+  let aux = -1                   // variavel q define variavel menos um
 
  
-  for (let i = 0; i < tarefas.length; i++) {
-    
-    if (tarefas[i].className == "tarefa-selecionada") {
-      tarefas[i].className = "tarefa-normal"      
+  for (let i = 0; i < tarefas.length; i++) {           // for para percorrer elementos 
+  
+    if (tarefas[i].className == "tarefa-selecionada") { // define posicao com tarefa selecionado
+      tarefas[i].className = "tarefa-normal"            // define posicao com tarefa-normal
       aux = i                                     
-      break                                       
+      break                                             // variavel q recebe posicao
     }
   }
 
   
-  if (aux == tarefas.length - 1) {
-    aux = -1
+  if (aux == tarefas.length - 1) {                      // se posicao == tamanho da tarefas 
+
+    aux = -1                                            // variavel decrementa em 1
   }
 
   tarefas[aux + 1].className = "tarefa-selecionada" 
 })
 
-frm.btRetirar.addEventListener("click", () => { 
-  const tarefas = document.querySelectorAll("h5") 
+frm.btRetirar.addEventListener("click", () => {         // evento retirar ao clickar
+  const tarefas = document.querySelectorAll("h3") 
 
   let aux = -1               
 
   
   tarefas.forEach((tarefa, i) => {
-    if (tarefa.className == "tarefa-selecionada") {  
+    if (tarefa.className == "tarefa-selecionada") {  // referencia tarefas  com a classe para dar retorno no pop-up
       aux = i
       console.log(i)                                   
     }
   })
 
   if (aux == -1) {             
-    alert("Selecione uma tarefa para removê-la...")
+    alert("Selecione uma tarefa para removê-la...")          //condicao para remover em 1 caso se aplique
     return
   }
 
@@ -67,10 +68,10 @@ frm.btRetirar.addEventListener("click", () => {
 })
 
 frm.btGravar.addEventListener("click", () => { 
-  const tarefas = document.querySelectorAll("h5")  
+  const tarefas = document.querySelectorAll("h3")    // evento clique para o botao tarefas gravar
 
   if (tarefas.length == 0) {
-    alert("Não há tarefas para serem salvas")      
+    alert("Não há tarefas para serem salvas")      // se nao tem tarefas retorna mensagem
     return                                         
   }
 
@@ -96,10 +97,40 @@ window.addEventListener("load", () => {
 
     // percorre os dados armazenados em localStorage
     dados.forEach(dado => {
-      const h5 = document.createElement("h5")      
+      const h3 = document.createElement("h3")      
       const texto = document.createTextNode(dado)  
-      h5.appendChild(texto)                      
-      dvQuadro.appendChild(h5)                   
+      h3.appendChild(texto)                      
+      dvQuadro.appendChild(h3)                   
     })
   }
 })
+
+
+frm.btGral.addEventListener("click", () => { 
+  const tarefas = document.querySelectorAll("h3")  // a intencao e criar um elemento onde separe as tarefas feitas das q estao para fazer
+
+  if (tarefas.length == 0) {
+    alert("Não há tarefas feitas")      
+    return                                         
+  }
+
+  let dados = ""                            
+  tarefas.forEach(tarefa => { 
+    dados += tarefa.innerText + ";"         
+  })
+
+  
+  localStorage.setItem("tarefasDia", dados.slice(0, -1))
+
+  
+  if (localStorage.getItem("tarefasDia")) {
+    alert("Ok! Tarefas Concuidas")
+  }
+})
+
+function mostrar_feitas()
+{
+  if (tarefas => 1)
+  localStorage.setItem("tarefasDia", dados.slice(0, -1))
+  return
+}
